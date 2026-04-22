c2f9fb47832c5e12fe37762616c81b016de00464
魏曹先生 <1992414357@qq.com>
2026-04-22 08:49:36 +0800

    Rename and refactor clippy scripts

--- dev_tools/scripts/clippy.sh
+++ dev_tools/scripts/clippy.sh
@@ -2,6 +2,5 @@
 
 find . -name "Cargo.toml" -type f | while read -r cargo_file; do
     project_dir=$(dirname "$cargo_file")
-    echo "Run \`cargo clippy\` in \`$project_dir\` ..."
-    (cd "$project_dir" && cargo clippy --fix --allow-dirty)
+    (cd "$project_dir" && cargo clippy --quiet)
 done
